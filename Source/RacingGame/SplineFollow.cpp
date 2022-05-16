2ec5f81792bd2d1d4771448f5b1814ddfa765835
haldorj <haldorjh@gmail.com>
2022-05-16 15:42:29 +0200

    1

--- Source/RacingGame/SplineFollow.cpp
+++ Source/RacingGame/SplineFollow.cpp
@@ -68,7 +68,7 @@ void ASplineFollow::Follow()
 	FVector XY = FVector(EnemyLocation.X- ArrowLocationWorld.X, EnemyLocation.Y- ArrowLocationWorld.Y, 0.0f);
 	float XYSize = XY.Size();
 
-	float MapRangeUnclamped = UKismetMathLibrary::MapRangeUnclamped(Speed*XYSize, 0, 2000, 1, 0.3);
+	float MapRangeUnclamped = UKismetMathLibrary::MapRangeUnclamped(Speed*XYSize, 0, 1000, 1, 0.5);
 	float select = UKismetMathLibrary::SelectFloat(1, MapRangeUnclamped, (Speed*XYSize) < 0);
 	EnemyRef->Throttle = UKismetMathLibrary::SelectFloat(1, select, XYSize < 0);
 
