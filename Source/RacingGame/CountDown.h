6ab674790d99f91153d649e1d39597bb27f59d19
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2022-05-18 11:05:58 +0200

    Revert "Countdown (almost) working, HUD not showing up"
    This reverts commit 9a5ec6ab6a3babbff655d1d8346b38fb116e9dc9.

--- Source/RacingGame/CountDown.h
+++ Source/RacingGame/CountDown.h
@@ -3,25 +3,51 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlayerCar.h"
+#include "CountDown.generated.h"
 
 
-/**
- * 
- */
-class RACINGGAME_API Countdown 
+class UTextRenderComponent;
+
+
+UCLASS()
+class RACINGGAME_API ACountDown : public AActor
 {
+	GENERATED_BODY()
+
 public:
-	Countdown();
-	~Countdown();
+	// Sets default values for this actor's properties
+	ACountDown();
 
-	bool IsCountingDown();
-	void SetCountingDown();
-	float GetSeconds();
-	
-	void CountDown(float DeltaTime);
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
-private:
-	bool CountingDown;
-	float SecondsLeft;
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerVariables")
+		UStaticMeshComponent* MeshComp = nullptr;
+
+	int32 CountdownTime;
+	UTextRenderComponent* CountdownText{ nullptr };
+
+	void UpdateTimerDisplay();
+
+	void AdvanceTimer();
+
+	void CountdownHasFinished();
+
+	FTimerHandle CountdownTimerHandle;
+
+	UPROPERTY(EditAnywhere)
+		USoundBase* CountDownSound {
+		nullptr
+	};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lights")
+		TArray<UStaticMeshComponent*> Lights;
 };
